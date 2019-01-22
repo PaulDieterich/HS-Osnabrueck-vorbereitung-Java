6b56c31ce6a1628020b97a8d025aabbd6764cea6
PaulDieterich <Paul.dieterich0@live.com>
2019-01-22 12:01:35 +0100

    Create FibonacciFolge.java

---
+++ hsosnabrueck/FibonacciFolge.java
@@ -0,0 +1,15 @@
+package hsosnabrueck;
+
+public class FibonacciFolge {
+	
+	public static void main(String[] args) {
+		int x = 1; 
+		int y = 0; 
+		
+		for(int i = 0; i < 50; i++) {
+			System.out.println(y);
+			y = x + (x = y);
+
+		}
+	}
+}
