1ec9f28a7b59e3723cfa7d44d57b2a6905ed7c6c
PaulDieterich <Paul.dieterich0@live.com>
2019-03-02 18:29:24 +0100

    FooBar + Math

---
+++ hsosnabrueck/Math_grundlagen.java
@@ -0,0 +1,27 @@
+package hsosnabrueck;
+
+import java.lang.*;
+
+public class Math_grundlagen {
+
+	public static void main(String[] args) {
+		int adition =  5 + 5; 
+		int subtration = 5 - 5;
+		int multiplikation = 5 * 5; 
+		int division = 5 / 5; 
+		
+		double squareRoot =  Math.sqrt(5);
+		double round = Math.round(5.4448);
+		double random = Math.random();
+	
+
+		System.out.println("adition = " + adition);
+		System.out.println("subtration = " + subtration);
+		System.out.println("multiplikation = " +multiplikation);
+		System.out.println("division = " +division);
+		System.out.println("squareRoot = " + squareRoot);
+		System.out.println("round = " + round);
+		System.out.println("random = " + random);
+	}
+
+}
---
+++ hsosnabrueck/fooBar.java
@@ -0,0 +1,25 @@
+package hsosnabrueck;
+
+public class fooBar {
+
+	public static void main(String[] args) {
+	
+		FooBar();
+	}
+	public static void FooBar() {
+		for(int i = 1; i <= 30; i++) {
+			if(i %3 == 0 && i % 5 == 0) {
+				System.out.println("foobar");
+			}else if( i %3 == 0) {
+				System.out.println("foo");
+			}else if( i % 5 == 0) {
+				System.out.println("bar");
+			}else {
+				System.out.println(i);
+			}
+		}
+
+	}
+}
+
+
